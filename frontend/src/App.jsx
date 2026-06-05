e120e9e34ef1dd11d387420f5af3343c7dbfa3e9
Ayman-Kz25 <kamalakz786@gmail.com>
2026-06-05 21:54:28 +0500

    routes created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,33 @@
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Orders from './pages/Orders';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/shop" element={<Shop/>} />
+        <Route path="/collection" element={<Collection/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/product/:productId" element={<Product/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/orders" element={<Orders/>} />
+      </Routes>
+    </div>
   )
 }
 export default App
