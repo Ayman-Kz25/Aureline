90236a75c3bf9a407273e2dc538df4848fbb75d8
Ayman-Kz25 <kamalakz786@gmail.com>
2026-06-05 14:48:51 +0500

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+export const products = [
+    {
+        _id: "1",
+        name: "",
+        description: "",
+        price: "",
+        image: "",
+        code: "",
+        tag: "",
+        color: [],
+        sizes: [],
+        date: "",
+        featured: false
+    }
+];
